dd073510a4ecbfbfd3e5c1d654577ff63687605f
programming6131 <kimutainashon@gmail.com>
2024-09-09 19:59:36 +0300

    push and fetch functions

--- storage.go
+++ storage.go
@@ -1,6 +1,30 @@
 package main
 
+import "fmt"
+
 type Storer interface {
 	Push([]byte) (int, error)
 	Fetch(int) ([]byte, error)
+}
+
+type MemoryStore struct {
+	data [][]byte
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make([][]byte, 0),
+	}
+}
+
+func (s *MemoryStore) Push(b []byte) error {
+	s.data = append(s.data, b)
+	return nil
+}
+
+func (s *MemoryStore) Fetch(offset int) ([]byte, error) {
+	if len(s.data) < offset {
+		return nil, fmt.Errorf("Offset (%d) is too high", offset)
+	}
+	return s.data[offset], nil
 }
